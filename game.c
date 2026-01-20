c2d6fdea0e035b390826d8856138e608554de78a
Parallels <jahsan.saleem@gmail.com>
2026-01-20 22:12:24 +0530

    grid making

--- game.c
+++ game.c
@@ -2,6 +2,25 @@
 
    int main() {
     printf("=== Welcome to SpyNet – The Codebreaker Protocol ===\n");
+
+    int n;
+    printf("Enter the grid size (5 <= N <= 15): ");
+    scanf("%d", &n);
+
+    
+    for (int j = 0; j < n; j++) printf(" __");
+    printf("\n");
+
+    
+    for (int i = 0; i < n; i++) {
+        
+        for (int j = 0; j < n; j++) printf("|  ");
+        printf("|\n");
+
+        
+        for (int j = 0; j < n; j++) printf("|__");
+        printf("|\n");
+    }
     
     return 0;
 }
